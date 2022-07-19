d066a2453797385805c4a5252758e1afa2be151f
thiyagu10 <thiyaguitx@gmail.com>
2022-07-19 14:10:49 +0530

    Update create_initramfs.sh

--- create_initramfs.sh
+++ create_initramfs.sh
@@ -2,10 +2,10 @@
 #                          INITRAMFS - BUSYBOX-1.34.1
 ##########################################################################################
 
-echo "Enter the INITRAMFS Build DIrectory [e.g. D10-BUILD] "
+echo "Enter the INITRAMFS Build Directory [e.g. D10-BUILD] "
 read rfsbuilddir
 
-echo "Enter the INITRAMFS Output DIrectory  [e.g. D10-RFS]"
+echo "Enter the INITRAMFS Output Directory  [e.g. D10-RFS]"
 read rfsoptdir
 
 echo "Enter the INITRAMFS BusyBOX version. [e.g. 1.34.1]"
@@ -15,41 +15,47 @@ XNETOS_BUILD=/opt/XNETOS
 KNL_VER=5.15.55
 
 sudo apt-get install libncurses5-dev -y
-mkdir -pv $XNETOS_BUILD
-
+if [ -d "$XNETOS_BUILD" ]
+then
+    echo "Directory exist. CONTINUE with this one"
+else
+   echo "Creating the XNETOS Directory"
+   mkdir -pv $XNETOS_BUILD
+fi
 if [ -n "$rfsbuilddir" -a -n "$rfsoptdir" ]
 then
     RFSBUILD=$HOME/$rfsbuilddir
-        RFSOPT=$HOME/$rfsoptdir
-        mkdir -p $RFSBUILD
-        if [ -f "/opt/zipimages/busybox-${bboxversion}.tar.bz2" ]
-        then
-            echo "busybox-${bboxversion}.tar.bz2 is found on /opt/zipimages. Using it to Local Repo"
-                cp /opt/zipimages/busybox-${bboxversion}.tar.bz2 $RFSBUILD
-        else
-            echo "busybox-${bboxversion}.tar.bz2 is not found on /opt/zipimages. Downloading it from Cloud Repo"
-                wget https://busybox.net/downloads/busybox-${bboxversion}.tar.bz2
-        fi
-        tar -xvf busybox-${bboxversion}.tar.bz2
-        cd busybox-${bboxversion}/
-        mkdir -pv $RFSOPT/obj/busybox-x86
-        make O=$RFSOPT/obj/busybox-x86 defconfig
-        make O=$RFSOPT/obj/busybox-x86 menuconfig
-        cd $RFSOPT/obj/busybox-x86
-        make -j $(nproc)
-        make install
-        mkdir -pv $RFSOPT/initramfs/x86-busybox
-        cd $RFSOPT/initramfs/x86-busybox
-        mkdir -pv {bin,dev,sbin,etc/network,proc,sys/kernel/debug,usr/{bin,sbin},lib,lib64,mnt/root,root,var{run,log},opt}
-        cp -av $RFSOPT/obj/busybox-x86/_install/*       $RFSOPT/initramfs/x86-busybox
-        cp -av /dev/{null,console,tty,ttyS0,sda1}       $RFSOPT/initramfs/x86-busybox/dev/
+    RFSOPT=$RFSBUILD/$rfsoptdir
+    mkdir -p $RFSBUILD
+    cd $RFSBUILD
+    if [ -f "/opt/LXKNL-REPO/busybox-${bboxversion}.tar.bz2" ]
+    then
+        echo "busybox-${bboxversion}.tar.bz2 is found on /opt/LXKNL-REPO. Using it to Local Repo"
+        cp /opt/LXKNL-REPO/busybox-${bboxversion}.tar.bz2 $RFSBUILD
+    else
+        echo "busybox-${bboxversion}.tar.bz2 is not found on /opt/LXKNL-REPO. Downloading it from Cloud Repo: https://busybox.net"
+        wget https://busybox.net/downloads/busybox-${bboxversion}.tar.bz2
+    fi
+    tar -xvf busybox-${bboxversion}.tar.bz2
+    cd busybox-${bboxversion}/
+    mkdir -pv $RFSOPT/obj/busybox-x86
+    make O=$RFSOPT/obj/busybox-x86 defconfig
+    make O=$RFSOPT/obj/busybox-x86 menuconfig
+    cd $RFSOPT/obj/busybox-x86
+    make -j $(nproc)
+    make install
+    mkdir -pv $RFSOPT/initramfs/x86-busybox
+    cd $RFSOPT/initramfs/x86-busybox
+    mkdir -pv {bin,dev,sbin,etc/network,proc,sys/kernel/debug,usr/{bin,sbin},lib,lib64,mnt/root,root,var/{run,log},opt}
+    cp -av $RFSOPT/obj/busybox-x86/_install/*   $RFSOPT/initramfs/x86-busybox
+    cp -av /dev/{null,console,tty,ttyS0,sda1}   $RFSOPT/initramfs/x86-busybox/dev/
 # Adding Init File
 cat <<EOF | sudo tee $RFSOPT/initramfs/x86-busybox/init
 #! /bin/sh
 mount -t proc none /proc
 mount -t sysfs none /sys
 mount -t debugfs none /sys/kernel/debug
-echo "\nBoot took $(cut -d' ' -f /proc/uptime) seconds\n"
+echo "XNETOS v1.0 is booted succesffully!!!"
 exec /bin/sh
 EOF
         chmod +x $RFSOPT/initramfs/x86-busybox/init
@@ -75,7 +81,7 @@ cat <<EOF | sudo tee $RFSOPT/initramfs/x86-busybox/etc/timezone
 Asia/Kolkata
 EOF
 
-# Adding OS-RELEASE File
+# Adding TimeZone File
 cat <<EOF | sudo tee $RFSOPT/initramfs/x86-busybox/etc/os-release
 NAME="XNETOS"
 ID=xnetos
@@ -84,7 +90,7 @@ PRETTY_NAME="XNETOS v1.0"
 HOME_URL="https://www.xnetworks.com/xnetos"
 BUG_REPORT_URL="https://www.xnetworks.com/xnetos/bugs"
 PRIVACY_POLICY_URL="https://www.xnetworks.com/xnetos/legel/terms-and-conditions/privacy-policy"
-VERSION_CODENAME=MUDHAL
+VERSION_CODENAME=0x00000B85
 EOF
         cd $RFSOPT/initramfs/x86-busybox/
         find . | cpio -H newc -o > ../initramfs.cpio
@@ -98,5 +104,5 @@ EOF
                 echo "PROBLEM in generating the INITRAMFS"
         fi
 else
-        echo "Inupt the correct directory to continue the INITRAMFS BUILD> Please rerun the script"
+        echo "Input the correct directory to continue the INITRAMFS BUILD. Please rerun the script"
 fi
