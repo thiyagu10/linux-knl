ff5f4b8a1940b3b34cc2bedbc0553a3fc41ae8a0
thiyagu10 <thiyaguitx@gmail.com>
2022-10-06 17:55:05 +0530

    Update build-linuxkernel.sh

--- build-linuxkernel.sh
+++ build-linuxkernel.sh
@@ -1,5 +1,5 @@
 #############################################################################################################
-#                               LINUX KENRLE 5.15.55 Compile on Ubuntu 20.04                                                                            #
+#                               LINUX KERNEL 5.x or 6.x Compile on Ubuntu 20.04                                                                         #
 #############################################################################################################
 
 echo "Enter the Linux Kernel Build Directory [e.g. LXKNL-BUILD] "
@@ -18,8 +18,9 @@ KNLBLDDIR=$HOME/$lxknlblddir
 KNLOUTDIR=$KNLBLDDIR/$lxkoutdir
 LOCALREPODIR=$localrepo
 KNL_VER=$lxkversion
-
-sudo update -y
+MAJOR_VER=$( echo "$KNL_VER" |cut -d\. -f1 )
+LNXKNL_URL='https://cdn.kernel.org/pub/linux/kernel/v'$MAJOR_VER'.x/linux-'$KNL_VER'.tar.xz'
+sudo apt update -y
 sudo apt-get install build-essential libncurses-dev libncurses5-dev linux-source libssl-dev libelf-dev bison flex bc kmod cpio dwarves -y
 sudo apt install wget htop qemu-system libvirt-clients libvirt-daemon-system virt-manager -y
 mkdir -p $KNLBLDDIR
@@ -31,15 +32,16 @@ then
         echo "linux-${KNL_VER}.tar.xz archive is available on local repo"
         cp $LOCALREPODIR/linux-${KNL_VER}.tar.xz $KNLBLDDIR
 else
-        echo "linux-${KNL_VER}.tar.xz archive is not available on local repo. Downloading it from https://www.kernel.org/"
-        wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${KNL_VER}.tar.xz
+        echo "linux-${KNL_VER}.tar.xz archive is not available on local repository. Downloading it from https://www.kernel.org/"
+        wget $LNXKNL_URL
+        cp linux-${KNL_VER}.tar.xz $LOCALREPODIR
 fi
 tar -xvf $KNLBLDDIR/linux-${KNL_VER}.tar.xz
 cd $KNLBLDDIR/linux-${KNL_VER}/
 make x86_64_defconfig
 make -j $(nproc)
 
-# x86_64 Image will be aviable on $KNLBLDDIR/linux-5.15.55/arch/x86/boot/bzImage
+# x86_64 Image will be aviable on $KNLBLDDIR/linux-{x.y.z}/arch/x86/boot/bzImage
 cp $KNLBLDDIR/linux-${KNL_VER}/arch/x86/boot/bzImage $KNLOUTDIR/vmlinuz-${KNL_VER}-generic
 cp $KNLBLDDIR/linux-${KNL_VER}/System.map $KNLOUTDIR/System.map-${KNL_VER}-generic
 cp $KNLBLDDIR/linux-${KNL_VER}/.config $KNLOUTDIR/config-${KNL_VER}-generic
