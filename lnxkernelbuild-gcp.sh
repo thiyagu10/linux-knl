f619ad183dab5e132bd96f6de079a063889625a1
thiyagu10 <thiyaguitx@gmail.com>
2022-10-09 13:31:36 +0530

    Update lnxkernelbuild-gcp.sh

--- lnxkernelbuild-gcp.sh
+++ lnxkernelbuild-gcp.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-sed -i '/PasswordAuthentication/s/no/yes/' /etc/ssh/sshd_config
+sudo sed -i '/PasswordAuthentication/s/no/yes/' /etc/ssh/sshd_config
 sudo systemctl restart sshd
 sudo timedatectl set-timezone Asia/Kolkata
 sudo apt update -y
