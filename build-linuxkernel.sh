af16f5f03c95bf4c70fc64b8a6c585ca89e9480a
thiyagu10 <thiyaguitx@gmail.com>
2022-10-09 10:16:11 +0530

    Update build-linuxkernel.sh

--- build-linuxkernel.sh
+++ build-linuxkernel.sh
@@ -1,15 +1,35 @@
+#!/bin/bash
 #############################################################################################################
 #                               LINUX KERNEL 5.x or 6.x Compile on Ubuntu 20.04                                                                         #
 #############################################################################################################
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -bdir parameterA -knlver parameterB -repo parameterC"
+   echo -e "\t-bdir Enter the Linux Kernel Build Directory [e.g. LXKNL-BUILD]"
+   echo -e "\t-knlver Enter the Linux Kernel Version. [e.g. 6.0]"
+   echo -e "\t-repo Enter the Linux Kernel Local Repo. [e.g. /opt/LXKNL-REPO]"
+   exit 1 # Exit script after printing help
+}
 
-echo "Enter the Linux Kernel Build Directory [e.g. LXKNL-BUILD] "
-read lxknlblddir
+while getopts ":d:v:r:" flags
+do
+   case "$flags" in
+      d )lxknlblddir=${OPTARG};;
+      v )lxkversion=${OPTARG};;
+      r )localrepo=${OPTARG};;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
 
-echo "Enter the Linux Kernel Version. [e.g. 6.0]"
-read lxkversion
+# Print helpFunction in case parameters are empty
+if [ -z "$lxknlblddir" ] || [ -z "$lxkversion" ] || [ -z "$localrepo" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
 
-echo "Enter the Linux Kernel Local Repo. [e.g. /opt/LXKNL-REPO]"
-read localrepo
+# Begin script in case all parameters are correct
 
 KNLBLDDIR=$HOME/$lxknlblddir
 KNLOUTDIR=$KNLBLDDIR/LXKNL-OUT
@@ -17,12 +37,21 @@ LOCALREPODIR=$localrepo
 KNL_VER=$lxkversion
 MAJOR_VER=6
 LNXKNL_URL='https://cdn.kernel.org/pub/linux/kernel/v'$MAJOR_VER'.x/linux-'$KNL_VER'.tar.xz'
+echo "Linux Kernel will be downloaded from $LNXKNL_URL"
+#echo "$KNLBLDDIR \t $KNLOUTDIR \t $LOCALREPODIR \t $KNL_VER \t $MAJOR_VER"
+#exit
 sudo apt update -y
 sudo apt-get install build-essential libncurses5-dev libssl-dev libelf-dev bison flex bc kmod cpio dwarves -y
 sudo apt install wget htop qemu-system libvirt-clients libvirt-daemon-system virt-manager -y
 mkdir -p $KNLBLDDIR
 mkdir -p $KNLOUTDIR
-
+if [ -d $LOCALREPODIR ]
+then
+	echo "Local Repository directory exist"
+else
+	echo "Local Repository directory doesn't exist. Creating it"
+	mkdir -p $LOCALREPODIR
+fi
 cd $KNLBLDDIR
 if [ -f "$LOCALREPODIR/linux-${KNL_VER}.tar.xz" ]
 then
@@ -42,5 +71,4 @@ make -j $(nproc)
 cp $KNLBLDDIR/linux-${KNL_VER}/arch/x86/boot/bzImage $KNLOUTDIR/vmlinuz-${KNL_VER}-generic
 cp $KNLBLDDIR/linux-${KNL_VER}/System.map $KNLOUTDIR/System.map-${KNL_VER}-generic
 cp $KNLBLDDIR/linux-${KNL_VER}/.config $KNLOUTDIR/config-${KNL_VER}-generic
-
 #############################################################################################################
