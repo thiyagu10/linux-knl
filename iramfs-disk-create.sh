81a42c89a68dbc50f1d5f31c9319d61ecbe13352
thiyagu10 <thiyaguitx@gmail.com>
2022-10-18 09:14:27 +0530

    Update iramfs-disk-create.sh

--- iramfs-disk-create.sh
+++ iramfs-disk-create.sh
@@ -149,7 +149,6 @@ sudo dd if=/dev/zero of=$NETWARE_BUILD/NETWARE-1-0_x86-64.img bs=1M count=256
 sudo mkfs -t ext4 $NETWARE_BUILD/NETWARE-1-0_x86-64.img
 sudo mkdir -p /mnt/VHD/
 sudo mount -t auto -o loop $NETWARE_BUILD/NETWARE-1-0_x86-64.img /mnt/VHD/
-sudo mkdir -p /mnt/VHD/boot
 cp -rf $RFSBUILD/$RFSOPT/initramfs/x86-busybox/* /mnt/VHD/
 cp $NETWARE_BUILD/initramfs-${KNL_VER}-generic /mnt/VHD/boot/
 cp $NETWARE_BUILD/vmlinuz-${KNL_VER}-generic /mnt/VHD/boot/
