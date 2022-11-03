ba8839985f87145a7d2aaf313bfa03a511656c62
thiyagu10 <thiyaguitx@gmail.com>
2022-11-03 08:01:13 +0530

    Update build-linuxkernel.sh

--- build-linuxkernel.sh
+++ build-linuxkernel.sh
@@ -1,51 +1,42 @@
 #!/bin/bash
 #############################################################################################################
-#                               LINUX KERNEL 5.x or 6.x Compile on Ubuntu 20.04                             
+#                               LINUX KERNEL 5.x or 6.x Compile on Ubuntu 20.04
 #############################################################################################################
-helpFunction()
-{
-   echo ""
-   echo "Usage: $0 -d parameterA -v parameterB -r parameterC"
-   echo -e "\t-d Enter the Linux Kernel Build Directory [e.g. LXKNL-BUILD]"
-   echo -e "\t-v Enter the Linux Kernel Version. [e.g. 6.0]"
-   echo -e "\t-r Enter the Linux Kernel Local Repo. [e.g. /opt/LXKNL-REPO]"
-   exit 1 # Exit script after printing help
-}
-while getopts ":d:v:r:" flags
-do
-   case "$flags" in
-      d )lxknlblddir=${OPTARG};;
-      v )lxkversion=${OPTARG};;
-      r )localrepo=${OPTARG};;
-      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
-   esac
-done
-# Print helpFunction in case parameters are empty
-if [ -z "$lxknlblddir" ] || [ -z "$lxkversion" ] || [ -z "$localrepo" ]
-then
-   echo "Some or all of the parameters are empty";
-   helpFunction
-fi
-KNLBLDDIR=$HOME/$lxknlblddir
-KNLOUTDIR=/opt/netware-1-x86-64
-LOCALREPODIR=$localrepo
-KNL_VER=$lxkversion
-MAJOR_VER=6
-LNXKNL_URL='https://cdn.kernel.org/pub/linux/kernel/v'$MAJOR_VER'.x/linux-'$KNL_VER'.tar.xz'
-echo "Linux Kernel will be downloaded from $LNXKNL_URL"
+
+export LOCALREPODIR="/opt/LNXKNL-REPO"
+export LNXIMAGEOUT="/opt/netware-1-x86-64"
+export LNXKNLBUILD="LNXKNL-BUILD"
+export KNL_VER="6.0.6"
+
+export KNLBLDDIR=$HOME/$LNXKNLBUILD
+export KNLOUTDIR=$KNLBLDDIR/LNXKNL-TEMP
+export MAJOR_VER=$(echo $KNL_VER | cut -d. -f1)
+export LNXKNL_URL=https://cdn.kernel.org/pub/linux/kernel/v$MAJOR_VER.x/linux-$KNL_VER.tar.xz
+
 sudo apt update -y
-sudo apt-get install build-essential libncurses5-dev libssl-dev libelf-dev bison flex bc kmod cpio dwarves -y
-sudo apt install wget htop qemu-system libvirt-clients libvirt-daemon-system virt-manager hexdump -y
-mkdir -pv $KNLBLDDIR
-mkdir -pv $KNLOUTDIR
+sudo apt install build-essential libncurses5-dev libssl-dev libelf-dev bison flex bc kmod cpio dwarves -y
+sudo apt install wget htop qemu-system libvirt-clients libvirt-daemon-system virt-manager -y
+
 if [ -d $LOCALREPODIR ]
 then
-	echo "Local Repository directory exist"
+        echo "Local Repository directory exist"
 else
-	echo "Local Repository directory doesn't exist. Creating it"
-	mkdir -p $LOCALREPODIR
+        echo "Local Repository directory doesn't exist. Creating it"
+        mkdir -pv $LOCALREPODIR
 fi
+
+if [ -d $LNXIMAGEOUT ]
+then
+        echo "Final Bootable Image Directory exist"
+else
+        echo "Final Bootable Image Directory doesn't exist. Creating it"
+        mkdir -pv $LNXIMAGEOUT
+fi
+
+mkdir -pv $KNLBLDDIR
+mkdir -pv $KNLOUTDIR
 cd $KNLBLDDIR
+
 if [ -f "$LOCALREPODIR/linux-${KNL_VER}.tar.xz" ]
 then
         echo "linux-${KNL_VER}.tar.xz archive is available on local repo"
@@ -55,12 +46,15 @@ else
         wget $LNXKNL_URL
         cp linux-${KNL_VER}.tar.xz $LOCALREPODIR
 fi
+
 tar -xvf $KNLBLDDIR/linux-${KNL_VER}.tar.xz
 cd $KNLBLDDIR/linux-${KNL_VER}/
 make x86_64_defconfig
 make -j $(nproc)
+
 # x86_64 Image will be aviable on $KNLBLDDIR/linux-{x.y.z}/arch/x86/boot/bzImage
-cp $KNLBLDDIR/linux-${KNL_VER}/arch/x86/boot/bzImage $KNLOUTDIR/vmlinuz-${KNL_VER}-generic
-cp $KNLBLDDIR/linux-${KNL_VER}/System.map $KNLOUTDIR/System.map-${KNL_VER}-generic
-cp $KNLBLDDIR/linux-${KNL_VER}/.config $KNLOUTDIR/config-${KNL_VER}-generic
+cp $KNLBLDDIR/linux-${KNL_VER}/arch/x86/boot/bzImage $LNXIMAGEOUT/vmlinuz-${KNL_VER}-generic
+cp $KNLBLDDIR/linux-${KNL_VER}/System.map $LNXIMAGEOUT/System.map-${KNL_VER}-generic
+cp $KNLBLDDIR/linux-${KNL_VER}/.config $LNXIMAGEOUT/config-${KNL_VER}-generic
+
 #############################################################################################################
