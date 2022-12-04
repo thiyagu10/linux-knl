e2b9e5982f1b70dd97c538201d41045b39203207
thiyagu10 <thiyaguitx@gmail.com>
2022-12-04 15:34:09 +0530

    Update build-linux-image.sh

--- build-linux-image.sh
+++ build-linux-image.sh
@@ -1,10 +1,10 @@
-sudo dd if=/dev/zero of=$NETWARE_BUILD/NETWARE-1-0_x86-64.img bs=1M count=256
-sudo mkfs -t ext4 $NETWARE_BUILD/NETWARE-1-0_x86-64.img
+sudo dd if=/dev/zero of=$LNXIMAGEOUT/NETWARE-1-0_x86-64.img bs=1M count=256
+sudo mkfs -t ext4 $LNXIMAGEOUT/NETWARE-1-0_x86-64.img
 sudo mkdir -p /mnt/VHD/
-sudo mount -t auto -o loop $NETWARE_BUILD/NETWARE-1-0_x86-64.img /mnt/VHD/
-cp -rf $RFSBUILD/$RFSOPT/initramfs/x86-busybox/* /mnt/VHD/
-cp $NETWARE_BUILD/initramfs-${KNL_VER}-generic /mnt/VHD/boot/
-cp $NETWARE_BUILD/vmlinuz-${KNL_VER}-generic /mnt/VHD/boot/
-cp $NETWARE_BUILD/config-${KNL_VER}-generic /mnt/VHD/boot/
-cp $NETWARE_BUILD/System.map-${KNL_VER}-generic /mnt/VHD/boot/
+sudo mount -t auto -o loop $LNXIMAGEOUT/NETWARE-1-0_x86-64.img /mnt/VHD/
+cp -rf $IRAMFSBUILD/final-ramvfs/* /mnt/VHD/
+cp $LNXIMAGEOUT/initramfs-${KNL_VER}-generic /mnt/VHD/boot/
+cp $LNXIMAGEOUT/vmlinuz-${KNL_VER}-generic /mnt/VHD/boot/
+cp $LNXIMAGEOUT/config-${KNL_VER}-generic /mnt/VHD/boot/
+cp $LNXIMAGEOUT/System.map-${KNL_VER}-generic /mnt/VHD/boot/
 sudo umount /mnt/VHD/
